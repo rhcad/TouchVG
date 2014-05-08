524bea46964787c16c27ae7fc8fa94767ad83e94
Zhang Yungui <rhcad@hotmail.com>
2014-05-08 16:42:59 +0800

    Add compile options for the Android x86 Emulator.

--- android/TouchVG/jni/Android.mk
+++ android/TouchVG/jni/Android.mk
@@ -1,7 +1,7 @@
 # Copyright (c) 2013-2014, Zhang Yungui, https://github.com/rhcad/touchvg
 #
 LOCAL_PATH := $(call my-dir)
-CORE_PATH  := ../../../../TouchVGCore/android/TouchVGCore/obj/local/$(TARGET_ARCH)
+CORE_PATH  := ../../../../TouchVGCore/android/TouchVGCore/obj/local/$(APP_ABI)
 CORE_INC   := $(LOCAL_PATH)/../../../../TouchVGCore/core/include
 
 include $(CLEAR_VARS)
